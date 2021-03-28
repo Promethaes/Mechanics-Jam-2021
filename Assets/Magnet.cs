49c2d1e678f3655f8f74bfc358eaa278085b3122
Anthony <anth19800@hotmail.com>
2021-03-27 20:17:54 -0400

    :m: :a: g n e :tm:

---
+++ Assets/Magnet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    public Material defaultMaterial;
+    public Material otherMaterial;
+    public bool startNegative = false;
+    public bool otherPolarity;
+
+
+    public void SwapPolarity()
+    {
+        Debug.Log("E");
+        otherPolarity = !otherPolarity;
+        if (otherPolarity)
+            gameObject.GetComponent<MeshRenderer>().material = otherMaterial;
+        else
+            gameObject.GetComponent<MeshRenderer>().material = defaultMaterial;
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "MiddleMagnet")
+        {
+
+            var rigid = other.gameObject.GetComponent<Rigidbody>();
+            //if (rigid.velocity.magnitude >= 20.0f)
+            //    rigid.velocity = Vector3.zero;
+
+            Vector3 dir = new Vector3();
+
+            if (startNegative)
+                dir = gameObject.transform.position - other.gameObject.transform.position;
+            else
+                dir = other.gameObject.transform.position - gameObject.transform.position;
+
+            dir = dir.normalized;
+            if (otherPolarity){
+                dir = -dir;
+                dir.x = 0.0f;
+            }
+
+            rigid.AddForce(dir, ForceMode.Impulse);
+        }
+    }
+
+}
---
+++ Assets/MagnetMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MagnetMovement : MonoBehaviour
+{
+    public GameObject topMag;
+    public GameObject bottomMag;
+
+    Vector2 topMove = Vector2.zero;
+    Vector2 bottomMove = Vector2.zero;
+
+    public float speed = 1.0f;
+
+    bool swap = false;
+
+    private void FixedUpdate()
+    {
+        if (swap)
+        {
+            topMag.GetComponent<Magnet>().SwapPolarity();
+            bottomMag.GetComponent<Magnet>().SwapPolarity();
+            swap = false;
+        }
+
+        topMag.transform.position = (topMag.transform.position + new Vector3(topMove.x, 0.0f, topMove.y) * speed * Time.fixedDeltaTime);
+        bottomMag.transform.position = (bottomMag.transform.position + new Vector3(bottomMove.x, 0.0f, bottomMove.y) * speed * Time.fixedDeltaTime);
+    }
+
+    public void OnTopMagnet(InputAction.CallbackContext ctx)
+    {
+        topMove = ctx.ReadValue<Vector2>();
+        topMove.y = 0.0f;
+    }
+    public void OnBottomMagnet(InputAction.CallbackContext ctx)
+    {
+        bottomMove = ctx.ReadValue<Vector2>();
+        bottomMove.y = 0.0f;
+    }
+
+    public void OnSwapPolarity(InputAction.CallbackContext ctx)
+    {
+        float button = ctx.ReadValue<float>();
+
+        swap = button > 0.5f ? swap = true : swap = false;
+
+
+    }
+}
